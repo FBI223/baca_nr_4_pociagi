2ed3798abbc4543b6e88f29043edf35fb976765f
Marcin <88114702+FBI223@users.noreply.github.com>
2022-12-11 22:47:00 -0500

    mp 4

---
+++ src/Source.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+public class Source {
+
+
+    public static Scanner skaner = new Scanner(System.in);
+
+    public static class Pociag
+    {
+
+    }
+
+    public static class Wagon
+    {
+
+    }
+
+    public static class Wagon_lista
+    {
+
+    }
+
+    public static class Lista_pociagow
+    {
+
+    }
+
+    public static class Dworzec
+    {
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
+
+
